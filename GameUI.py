17425adc95e3dde57927f714eaf431be388d316e
Katsumi Kougen <lesanonyme1001@gmail.com>
2023-01-25 22:24:13 +0700

    New rendering system

--- GameUI.py
+++ GameUI.py
@@ -2,13 +2,15 @@ from IPython import display
 import re
 import random
 
-class Renderer:
-    
-    def __init__(self):
-        self.RendererID = random.randint(0, 0xffff)
+class CoreRenderer:
 
     def DisplayMd(self, text: str):
         display.display_markdown(text, raw=True)
 
     def DisplayHTML(self, text: str):
         display.display_html(text, raw=True)
+
+class Renderer(CoreRenderer):
+    
+    def __init__(self):
+        self.RendererID = random.randint(0, 0xffff)
