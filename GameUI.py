cf831d84e30c1bba40bc72ae31fc60b7852ad056
Katsumi Kougen <lesanonyme1001@gmail.com>
2023-01-26 21:23:50 +0700

    Add menu ("condensed" type and customisable "mutable" type)

--- GameUI.py
+++ GameUI.py
@@ -1,6 +1,13 @@
 from IPython import display
-import re
+from typing import Optional, Union
 import random
+    
+# Constances
+CONDENSED_MENU = 0
+MUTABLE_MENU = 1
+
+class EngineError:
+    pass
 
 class CoreRenderer:
 
@@ -10,7 +17,79 @@ class CoreRenderer:
     def DisplayHTML(self, text: str):
         display.display_html(text, raw=True)
 
+class DisplayBlock:
+    
+    def __init__(self):
+        self.Elements = {}
+        self.mutable = True
+        self._DisplayBlockID = random.randint(0, 0xffffffff)
+    
+    def ModifyElement(self, element: str, text: str) -> None | EngineError:
+        if element in self.Elements.keys() or self.mutable:
+            self.Elements[element] = text
+    
+    def RemoveElement(self, element: str):
+        self.Elements.pop(element)
+    
+    def PopElement(self, element: str) -> tuple:
+        return (element, self.Elements.pop(element))
+
+class Menu(DisplayBlock):
+    
+    def __init__(self, type_: int):
+        super().__init__()
+        match type_:
+            case 0:
+                self.mutable = False
+                self.Elements["header"] = "# Your Game"
+                self.Elements["commands"] = \
+                    "New game &#x2014; `Player.NewGame()`\n" \
+                    "\n" \
+                    "Load game &#x2014; `Player.Load()`"
+                self.Elements["footer"] = \
+                    "----\n" \
+                    "Created by me"
+            case 1:
+                self.Elements["header"] = "# Your Game"
+                self.Elements["commands"] = \
+                    "New game &#x2014; `Player.NewGame()`\n" \
+                    "\n" \
+                    "Load game &#x2014; `Player.Load()`"
+                self.Elements["footer"] = \
+                    "----\n" \
+                    "Created by me"
+
 class Renderer(CoreRenderer):
+
+    # Error codes
+    class ErrorCode:
+        Index = 0x22
     
     def __init__(self):
         self.RendererID = random.randint(0, 0xffff)
+
+    def ShowError(self, errcode: int, sender: str = "Engine", message: str = "No message set"):
+        self.DisplayMd(
+            "### **<div style=\"background: red; color: white;\">Error!</div>**\n"
+            f"{message}"
+            "\n"
+            "\n"
+            "|Error code|Caught by|\n"
+            "|:-|:-|\n"
+            f"|`{errcode}`|{sender}|\n"
+            "\n"
+            "----"
+        )
+    
+    def SetMenu(self, extra_elements: Optional[dict[str, str]] = None, menutype: int = 0):
+        self.Menu = Menu(menutype)
+        if extra_elements is not None:
+            for k, v in zip(extra_elements.keys(), extra_elements.values()):
+                if not self.Menu.mutable:
+                    self.ShowError(self.ErrorCode.Index, message=f"The `CONDENSED_MENU` type menu is not mutable")
+                else:
+                    self.Menu.ModifyElement(k, v)
+    
+    def DisplayMenu(self):
+        for i in self.Menu.Elements.values():
+            self.DisplayMd(i)
